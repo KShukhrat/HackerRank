fcc444a1b547127efb41612a09289d845a3bd668
shuhrat <qayumov7298668@gmail.com>
2023-06-07 17:58:11 +0500

    service and repository

--- src/main/java/uz/pdp/hackerrank/repository/UserRepository.java
+++ src/main/java/uz/pdp/hackerrank/repository/UserRepository.java
@@ -1,9 +1,8 @@
 package uz.pdp.hackerrank.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import uz.pdp.hackerrank.entity.user.UserEntity;
 
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+public interface UserRepository extends JpaRepository<UserRepository, UUID> {
 }
