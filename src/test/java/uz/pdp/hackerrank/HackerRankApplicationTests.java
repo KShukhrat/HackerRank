55bc9f073cdebb646faa9777c5cc6a21bbbda982
shuhrat <qayumov7298668@gmail.com>
2023-06-07 16:57:53 +0500

    start

---
+++ src/test/java/uz/pdp/hackerrank/HackerRankApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.hackerrank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackerRankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
