5fc888f7967ee7ba725bda790299b087e7a50ac3
bobur <sobirjanovbobur768@gmail.com>
2023-06-07 18:27:40 +0500

    user repository changed

--- src/main/java/uz/pdp/hackerrank/repository/UserRepository.java
+++ src/main/java/uz/pdp/hackerrank/repository/UserRepository.java
@@ -2,8 +2,9 @@ package uz.pdp.hackerrank.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import uz.pdp.hackerrank.entity.user.UserEntity;
 
 import java.util.UUID;
 @Repository
-public interface UserRepository extends JpaRepository<UserRepository, UUID> {
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 }
