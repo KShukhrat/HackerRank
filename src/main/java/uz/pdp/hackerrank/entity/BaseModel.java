536ceca4810f20294e0f64ca08b7e122d8291948
Yusupov Ulug`bek <yusupovulugbek73@gmail.com>
2023-06-07 17:29:00 +0500

    ggg

--- src/main/java/uz/pdp/hackerrank/entity/BaseModel.java
+++ src/main/java/uz/pdp/hackerrank/entity/BaseModel.java
@@ -1,12 +1,20 @@
 package uz.pdp.hackerrank.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.UUID;
 
 @MappedSuperclass
 @Getter
 @Setter
 public abstract class BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected UUID id;
 
 }
--- src/main/java/uz/pdp/hackerrank/entity/user/UserEntity.java
+++ src/main/java/uz/pdp/hackerrank/entity/user/UserEntity.java
@@ -1,5 +1,15 @@
 package uz.pdp.hackerrank.entity.user;
 
-public class UserEntity {
+import jakarta.persistence.Entity;
+import lombok.*;
+import uz.pdp.hackerrank.entity.BaseModel;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class UserEntity extends BaseModel {
 
 }
