b3fae49ed8161ede316ab8d15eaf6b1a72dc0817
bobur <sobirjanovbobur768@gmail.com>
2023-06-09 16:21:34 +0500

    quescontrollerda ozgarish ques repo da ozgarish va ques service servisimplda ozgarish 16:21

--- src/main/java/uz/pdp/hackerrank/repository/QuestionRepository.java
+++ src/main/java/uz/pdp/hackerrank/repository/QuestionRepository.java
@@ -3,9 +3,15 @@ package uz.pdp.hackerrank.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uz.pdp.hackerrank.entity.question.QuestionEntity;
+import uz.pdp.hackerrank.entity.question.QuestionTheme;
+import uz.pdp.hackerrank.entity.question.QuestionType;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface QuestionRepository  extends JpaRepository<QuestionEntity, UUID>{
+
+    List<QuestionEntity> findQuestionEntitiesByThemeEqualsIgnoreCaseAndQuestionTypeContainsIgnoreCase(QuestionTheme theme, QuestionType type);
+
 }
