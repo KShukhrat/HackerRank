7890f08520eacfe4f6f05fa4669910b1853d8574
shuhrat <qayumov7298668@gmail.com>
2023-06-12 14:23:54 +0500

    not blank in UserCreateDto

--- src/main/java/uz/pdp/hackerrank/entity/dto/UserCreateDto.java
+++ src/main/java/uz/pdp/hackerrank/entity/dto/UserCreateDto.java
@@ -13,10 +13,9 @@ import jakarta.validation.constraints.NotNull;
 public class UserCreateDto {
     private String name;
     @NotBlank(message = "username cannot be blank or empty")
-    @NotNull(message = "username cannot be null")
     private String username;
     @NotBlank(message = "password cannot be blank or empty")
-    @NotNull(message = "password cannot be null")
+
     private String password;
     private List<UserRole> roles;
 }
