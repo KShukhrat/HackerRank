b180b91e2b0ce76279ab61db8f7a496057f4b7f4
shuhrat <qayumov7298668@gmail.com>
2023-06-12 17:43:23 +0500

    getById QuestionService

--- src/main/java/uz/pdp/hackerrank/service/question/QuestionServiceImpl.java
+++ src/main/java/uz/pdp/hackerrank/service/question/QuestionServiceImpl.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import uz.pdp.hackerrank.entity.UserQuestion;
 import uz.pdp.hackerrank.entity.dto.QuestionCreateDto;
@@ -13,7 +12,6 @@ import uz.pdp.hackerrank.entity.question.QuestionTheme;
 import uz.pdp.hackerrank.entity.question.QuestionType;
 import uz.pdp.hackerrank.entity.user.UserEntity;
 import uz.pdp.hackerrank.exception.DataNotFoundException;
-import uz.pdp.hackerrank.exception.RequestValidationException;
 import uz.pdp.hackerrank.repository.QuestionRepository;
 import uz.pdp.hackerrank.repository.UserRepository;
 import uz.pdp.hackerrank.service.user.UserService;
@@ -22,6 +20,7 @@ import uz.pdp.hackerrank.service.user_question.UserQuestionService;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -72,6 +71,11 @@ public class QuestionServiceImpl implements QuestionService{
             questions.add(questionRepository.findQuestionEntityById(userQuestion.getQuestion().getId()));
         }
         return questions;
+    }
+
+    @Override
+    public Optional<QuestionEntity> getById(UUID id) {
+        return questionRepository.findById(id);
     }
 
     public List<QuestionEntity> findByThemeAndType(QuestionTheme theme, QuestionType type){
